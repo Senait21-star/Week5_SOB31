7dd867c7293b6617a81b544d393482edf04f2708
Senait21-star <SA3698@live.mdx.ac.uk>
2025-04-10 18:02:25 +0400

    Update broken_grades.py

--- broken_grades.py
+++ broken_grades.py
@@ -1,4 +1,4 @@
-# Calculating Grades (ok, let me think about this one)
+hh# Calculating Grades (ok, let me think about this one)
 
 # Write a program that will average 3 numeric exam grades, return an average test score, a corresponding letter grade, and a message stating whether the student is passing.
 
